0018ac5962cc7a83a4a6331874f1f7eac8c45758
Chase <chase@csh.rit.edu>
2016-03-24 23:34:20 -0400

    Initial commit

---
+++ app/src/main/java/edu/chase/csh/taladitepricecaculator/adapters/ItemAdapter.kt
@@ -0,0 +1,39 @@
+package edu.chase.csh.taladitepricecaculator.adapters
+
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import edu.chase.csh.taladitepricecaculator.R
+import edu.chase.csh.taladitepricecaculator.managers.ItemManager
+import org.jetbrains.anko.find
+
+class ItemAdapter(val items: ItemManager, val parent: Context) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.name = item.name
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(this.parent).inflate(R.layout.wow_item_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount() = items.size
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        private val nameField = v.find<TextView>(R.id.wow_item_name_text)
+        var name: CharSequence
+            get() = nameField.text
+            set(s: CharSequence) {
+                nameField.text = s
+            }
+
+    }
+
+}
---
+++ app/src/main/java/edu/chase/csh/taladitepricecaculator/managers/ItemManager.kt
@@ -0,0 +1,10 @@
+package edu.chase.csh.taladitepricecaculator.managers
+
+import edu.chase.csh.taladitepricecaculator.models.Item
+import java.util.*
+
+//Eventually load on demand
+class ItemManager : ArrayList<Item>() {
+
+
+}
